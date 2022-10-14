e0df98c9a21b02dfa27f320b79eaab1a3911c476
Marcus Graciano <marcusgraciano.ads@outlook.com>
2022-10-14 00:17:39 -0300

    Add Forms2 to 9-HTML

--- 9-html/exercicios/server.js
+++ 9-html/exercicios/server.js
@@ -6,7 +6,18 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body);
-    resp.send('<h1>Parabéns</h1>')
+    resp.send('<h1>Parabéns. Usuário incluido</h1>')
+})
+
+// app.get('/usuarios', (req, resp) => {
+//     console.log(req.query);
+//     resp.send('<h1>Parabéns. Usuário incluido</h1>')
+// })
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.body);
+    console.log(req.params.id);
+    resp.send('<h1>Parabéns. Usuário alterado</h1>')
 })
 
 app.listen(8080)
